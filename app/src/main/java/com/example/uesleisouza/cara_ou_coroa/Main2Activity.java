dc6bb482164e46197884e1947398e5536d8e7cd9
Ueslei Stevans Fonseca de Souza <ueslei.souza@cwi.com.br>
2017-09-29 12:30:10 -0300

    Initial commit

---
+++ app/src/main/java/com/example/uesleisouza/cara_ou_coroa/Main2Activity.java
@@ -0,0 +1,33 @@
+package com.example.uesleisouza.cara_ou_coroa;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+
+    private TextView text;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        text = (TextView) findViewById(R.id.textView);
+        button = (Button) findViewById(R.id.button2);
+
+        Bundle extra = getIntent().getExtras();
+        if(extra != null) text.setText(extra.getString("result"));
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Main2Activity.this, MainActivity.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/uesleisouza/cara_ou_coroa/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.uesleisouza.cara_ou_coroa;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private String[] option = {"Cara","Coroa"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Random random = new Random();
+                int position = random.nextInt(2);
+
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                intent.putExtra("result", option[position]);
+
+                startActivity(intent);
+            }
+        });
+    }
+}
